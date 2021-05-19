cb4b6699fbee8595cf82115a763a6ca34236e084
Emerson Martins <emersonmartinsdedeus1@gmail.com>
2021-05-19 01:50:22 -0300

    commit on 'master'

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categorias_meals_screen.dart';
+
+import 'screens/categories_screen.dart';
+import 'screens/categorias_meals_screen.dart';
+import 'utils/app_routes.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.pinkAccent,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          headline6: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondesed',
+          )
+        )
+      ),
+      routes: {
+        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+      },
+    );
+  }
+}
