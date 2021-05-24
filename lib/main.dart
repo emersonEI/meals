12f8b1f27ff00ae2fa6be486b34b9bf072a0f37b
Emerson Martins <emersonmartinsdedeus1@gmail.com>
2021-05-24 05:37:38 -0300

    commit on 'master'

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:meals/screens/categorias_meals_screen.dart';
 
 import 'screens/categories_screen.dart';
-import 'screens/categorias_meals_screen.dart';
+import 'screens/categories_meals_screen.dart';
+import 'screens/meal_detail_screen.dart';
 import 'utils/app_routes.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.pinkAccent,
+        primarySwatch: Colors.red,
+        accentColor: Colors.orangeAccent,
         fontFamily: 'Raleway',
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         textTheme: ThemeData.light().textTheme.copyWith(
-          headline6: TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondesed',
-          )
-        )
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondesed',
+              ),
+            ),
       ),
       routes: {
         AppRoutes.HOME: (ctx) => CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
       },
     );
   }
--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,4 +1,5 @@
 class AppRoutes {
   static const HOME = '/';
   static const CATEGORIES_MEALS = '/categories-meal';
+  static const MEAL_DETAIL = '/meal-detail';
 }
